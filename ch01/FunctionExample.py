4582075fa9dcd6c98dc7fcd4725c68e0e82c1a52
iksflow <theiksflow@gmail.com>
2020-11-29 22:31:01 +0900

    Study function usages

--- ch01/FunctionExample.py
+++ ch01/FunctionExample.py
@@ -15,3 +15,27 @@ sayHi()
 def addNumberThenPrint(a, b):
     print("%d + %d = %d" %(a, b, a+b))
 print(addNumberThenPrint(3, 4))
+
+## 매개변수 지정해서 호출하기
+def printAThenB(a, b):
+    return "%s then %s" % (a, b)
+
+print("printAThenB(3,7) =", printAThenB(3, 7))
+print("printAThenB(b=7, a=3)", printAThenB(b=7, a=3))
+
+## 인자가 여러개 있을경우
+def add_many(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result
+print("add_many(1,2,3) = ", add_many(1,2,3))
+print("add_many(1,2,3,4,5,6) = ", add_many(1,2,3,4,5,6))
+
+# 키워드 파라미터
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+
+print_kwargs(a=1)
+print_kwargs(name='foo', age=3)
