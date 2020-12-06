44bfb705a26d36921622fa374acf7737bbb66681
iksflow <theiksflow@gmail.com>
2020-12-06 22:07:53 +0900

    2020.12.06 : Study file I/O

--- ch01/FileInputOutputExample.py
+++ ch01/FileInputOutputExample.py
@@ -22,3 +22,25 @@ lines = f.readlines()
 for i in lines:
     print(i)
 f.close()
+
+# 파일 내용을 문자열로 반환
+f = open("C:/doit/새파일.txt", "r")
+data = f.read()
+print(data)
+f.close()
+
+# 파일에 내용 추가하기
+f = open("C:/doit/새파일.txt", "a")
+for i in range(11, 21):
+    data = "%d 번째 줄입니다.\n" % i
+    f.write(data)
+
+f.close
+
+
+# with문을 사용해 파일 닫기 자동화하기. with문을 벗어나는 순간 자동으로 close된다.
+
+with open("C:/doit/새파일2.txt", "w") as f:
+    f.write("Life is too short, you need python")
+
+
---
+++ ch01/SystemArgsExample.py
@@ -0,0 +1,4 @@
+import sys
+args = sys.argv[0:]
+for i in args:
+    print(i.upper(), end = ' ')
