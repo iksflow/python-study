9e55002d2e551a1471958d6a1a7c598541f43591
iksflow <theiksflow@gmail.com>
2020-12-11 23:35:23 +0900

    2020.12.11 : Study class variable

--- ch05/ClassInheritanceExample.py
+++ ch05/ClassInheritanceExample.py
@@ -43,3 +43,29 @@ print("a.mul() = ", a.mul())
 print("a.sub() = ", a.sub())
 print("a.div() = ", a.div())
 print("a.pow() = ", a.pow())
+b = SafeFourCal(4, 0)
+# overriding 된 함수가 호출된다.
+print("b.div() = ", b.div())
+
+# 클래스변수 정의하기
+class Family:
+    lastname = "김"
+
+# 클래스를 통해 호출하는 방법
+print("Family.lastname =", Family.lastname)
+f1 = Family()
+f2 = Family()
+# 인스턴스를 통해 호출하는 방법
+print("f1.lastname = ", f1.lastname)
+print("f2.lastname = ", f2.lastname)
+
+# 클래스 변수 값 변경하기
+Family.lastname = "박"
+
+# 생성된 모든 객체의 lastname이 변경됐다.
+print("f1.lastname = ", f1.lastname)
+print("f2.lastname = ", f2.lastname)
+
+# id로 비교해보면 같은 메모리주소를 참조하고있는것을 알 수 있다.
+print("id(f1.lastname) = ", id(f1.lastname))
+print("id(f2.lastname) = ", id(f2.lastname))
